fa7439e906f4e7b28a143fd66c01163edac20f1e
Aya ElNahas <ayaa.alnahas@gmail.com>
2019-11-15 12:39:03 +0200

    re-commiting structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './Store/Reducers/rootReducer';
+import './index.css';
+import App from './APP/App';
+import * as serviceWorker from './serviceWorker';
+import 'element-theme-default';
+import {Button} from 'element-react/next'
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+ReactDOM.render(<>
+    <Provider store={store}><App /></Provider>
+    <Button type="primary">Hello</Button>
+</>
+    , document.getElementById('root'));
+
+//App.use(cors())
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
